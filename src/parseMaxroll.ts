bb186976230d53df7f6fddd9ebeba1573a56b226
Espen Jacobsson <espenjacobsson@gmail.com>
2021-04-11 17:00:25 +0200

    Dump jsdom in favor of cheerio

--- src/parseMaxroll.ts
+++ src/parseMaxroll.ts
@@ -1,23 +1,24 @@
 import got from "got"
 import colors from "colors"
-import { JSDOM } from "jsdom"
+import cheerio from "cheerio"
 
 export const getMaxRollVariants = async (url: string) => {
 	console.log(colors.green(`Loading build from ${colors.reset(url)}`))
 
 	const page = await got(url)
-	const dom = new JSDOM(page.body)
-	const variants = dom.window.document.querySelectorAll(".advgb-tab-body-container") || []
+	const $ = cheerio.load(page.body)
+	const variants = $(".advgb-tab-body-container")
 	const setInfos = []
 
 	for (const variant of variants) {
 		if (!variant) {
 			continue
 		}
-		const setName = variant.querySelector(".advgb-tab-body-header")?.textContent
-		const build = variant.querySelector(".d3planner-build")
-		const buildId = build?.getAttribute("data-d3planner-id")
-		const setId = parseInt(build?.getAttribute("data-d3planner-set") as string)
+
+		const setName = $(".advgb-tab-body-header", variant).text()
+		const build = $(".d3planner-build", variant)
+		const buildId = build.attr("data-d3planner-id")
+		const setId = parseInt(build.attr("data-d3planner-set") as string)
 
 		if (!setName || !build || !buildId) {
 			continue
